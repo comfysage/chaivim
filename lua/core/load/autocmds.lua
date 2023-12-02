3a2237e5f80117f07e745c7b9c2752694eee2baf
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2023-12-02 12:19:56 +0100

    add(core.load.autocmds): add create_reload fn

--- lua/core/load/autocmds.lua
+++ lua/core/load/autocmds.lua
@@ -1,3 +1,5 @@
+local parts = require 'core.parts'
+
 return {
   setup = function(opts)
     vim.api.nvim_create_autocmd('BufWritePost', {
@@ -9,6 +11,20 @@ return {
       group = opts.group_id,
       pattern = 'lua/' .. CONFIG_MODULE .. '/init.lua',
       desc = 'config:reload:' .. CONFIG_MODULE,
+    })
+  end,
+  ---@param spec ModuleSpec
+  create_reload = function (module, spec)
+    local file_name = string.gsub(module, '[.]', '/')
+    vim.api.nvim_create_autocmd('BufWritePost', {
+      callback = function (props)
+        vim.notify('reloading module: ' .. module .. '\t' .. props.file, vim.log.levels.INFO)
+        package.loaded[module] = nil
+        parts.load(module, spec)
+      end,
+      group = core.group_id,
+      pattern = 'lua/' .. file_name .. '.lua',
+      desc = 'config:reload:' .. module,
     })
   end,
 }
