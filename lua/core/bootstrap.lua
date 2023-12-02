c21df5da29b11dfb0ddc86a59656261110fde0ca
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2023-12-02 10:30:28 +0100

    add(core.bootstrap): modularize `core.bootstrap`

--- lua/core/bootstrap.lua
+++ lua/core/bootstrap.lua
@@ -1,24 +1,45 @@
+local Util = require 'core.utils'
+
+---@type { [string]: { load: function } }
+local fn = {
+  keymaps = {
+    load = function()
+      local keymapspath = core.path.keymaps
+
+      vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/crispybaccoon/keymaps.nvim.git",
+        keymapspath,
+      })
+
+      local ok, keymaps_nvim = pcall(R, 'keymaps')
+      if not ok then
+        return false
+      end
+      return keymaps_nvim
+    end,
+  },
+}
+
+---@param name string
+---@param props string
+---@return function|nil
+local function _get(name, props)
+  local _fn = fn[name]
+  if not _fn or not _fn[props] then
+    Util.log('no bootstrap functions found for: ' .. name, 'error')
+    return
+  end
+  return _fn[props]
+end
+
 return {
   load = function(props)
-    local fn = {
-      keymaps = function()
-        local keymapspath = core.path.keymaps
-
-        vim.fn.system({
-          "git",
-          "clone",
-          "--filter=blob:none",
-          "https://github.com/crispybaccoon/keymaps.nvim.git",
-          keymapspath,
-        })
-
-        local ok, keymaps_nvim = pcall(R, 'keymaps')
-        if not ok then
-          return false
-        end
-        return keymaps_nvim
-      end
-    }
-    fn[props]()
+    local _fn = _get(props, 'load')
+    if _fn then
+      _fn()
+    end
   end
 }
