0e6d88b96271936ccd396674d2e0e246b0fd50c8
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2023-12-04 10:37:06 +0100

    feat(core.config.telescope): create telescope config module

---
+++ lua/core/config/telescope.lua
@@ -0,0 +1,48 @@
+local M = {}
+
+---@alias TelescopeConfig {}
+
+---@type TelescopeConfig
+local default_config = {
+  defaults = {
+    prompt_prefix = '$ ',
+    file_previewer = require 'telescope.previewers'.vim_buffer_cat.new,
+    path_display = nil,
+    file_ignore_patterns = { 'node_modules', '%.git/' },
+    borderchars = {
+      { '─', '│', '─', '│', '┌', '┐', '┘', '└' },
+      prompt = { "─", "│", "─", "│", '┌', '┐', "┘", "└" },
+      results = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
+      preview = { '─', '│', '─', '│', '┌', '┐', '┘', '└' },
+    },
+  },
+  extensions = {
+    fzf = {
+      fuzzy = true,                         -- false will only do exact matching
+      override_generic_sorter = true,       -- override the generic sorter
+      override_file_sorter = true,          -- override the file sorter
+      case_mode = 'smart_case',             -- or 'ignore_case' or 'respect_case', defaults to 'smart_case'
+    },
+  },
+  pickers = {
+    find_files = {
+      disable_devicons = false,
+    },
+  },
+}
+
+---@class CoreTelescopeOpts
+---@field config TelescopeConfig
+
+---@param opts CoreTelescopeOpts
+M.setup = function(opts)
+  local config = vim.tbl_deep_extend('force', default_config, opts.config or {})
+
+  require 'telescope'.setup (config)
+
+  if config.use_fzf then
+    require 'telescope'.load_extension 'fzf'
+  end
+end
+
+return M
