dffdc76dd1f0f39340e29fc9e598ab0bab2e0081
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2023-12-11 16:43:58 +0100

    fix(lazy.plugins): add telescope dependencies

--- lua/core/lazy/plugins.lua
+++ lua/core/lazy/plugins.lua
@@ -2,7 +2,12 @@ return {
   { name = 'chai', 'crispybaccoon/chaivim', enabled = false },
   { name = 'keymaps', 'crispybaccoon/keymaps.nvim' },
   { name = 'plenary', 'nvim-lua/plenary.nvim' },
-  { name = 'telescope', 'nvim-telescope/telescope.nvim' },
+  { name = 'telescope', 'nvim-telescope/telescope.nvim',
+    dependencies = {
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+      'sharkdp/fd',
+    },
+  },
   { name = 'lualine', 'nvim-lualine/lualine.nvim' },
   { name = 'evergarden', 'crispybaccoon/evergarden' },
   { import = core.modules.core.lazy.opts.module },
