a12d2dc00a7135739830bc4606c422548f7ce44d
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2023-12-15 14:20:29 +0100

    fix(core.config.lsp): use qf list for diagnostics instead of loclist

--- lua/core/config/lsp.lua
+++ lua/core/config/lsp.lua
@@ -67,7 +67,7 @@ return {
     vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
     vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
     vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-    vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+    vim.keymap.set('n', '<space>q', vim.diagnostic.setqflist)
 
     -- nvim-cmp supports additional completion capabilities
     local capabilities = vim.lsp.protocol.make_client_capabilities()
