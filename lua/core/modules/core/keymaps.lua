384636e170d932c9ea4b3edc7d6aa84e49bb2c4f
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2023-12-16 11:27:36 +0100

    feat(core.modules.core.keymaps): add default keymaps

--- lua/core/modules/core/keymaps.lua
+++ lua/core/modules/core/keymaps.lua
@@ -3,7 +3,31 @@ return {
     opts = {
       leader = 'SPC',
       defaults = {},
-      mappings = {},
+      mappings = {
+        normal = {
+          -- tab switching
+          ['<TAB>'] = { vim.cmd.tabnext, 'Next Tab' },
+          ['<S-TAB>'] = { vim.cmd.tabprev, 'Prev Tab' },
+          ['<space><TAB>'] = { ':$tabedit<CR>', 'Open New Tab' },
+          -- quick fix list
+          ['<c-n>'] = { ':cnext<cr>', 'goto next item in qf list' },
+          ['<c-b>'] = { ':cprev<cr>', 'goto prev item in qf list' },
+          -- copy/pasting from system clipboard
+          ['<c-v>'] = { '"+p', 'paste from system clipboard' },
+          -- < and > indents
+          ['<'] = { '<gv', 'Decrease indention' },
+          ['>'] = { '>gv', 'Increase indention' },
+          [',tb'] = { function()
+            ---@diagnostic disable
+            _G.toggle_transparent_background()
+          end, 'toggle transparent background' },
+          ['<C-\\>'] = { ':vs<CR>:wincmd l<CR>', 'Split File Vertically' },
+        },
+        visual = {
+          -- copy/pasting from system clipboard
+          ['<c-c>'] = { '"+y', 'copy to system clipboard' },
+        },
+      },
     },
   },
 }
