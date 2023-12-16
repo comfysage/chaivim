a71721491d58da627fb774e5042b84ccc3fb1fb4
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2023-12-16 09:47:49 +0100

    create simple starter template.

---
+++ init.lua
@@ -0,0 +1,16 @@
+-- bootstrap chaivim
+local rootpath = vim.fn.stdpath('data') .. '/core'
+local chaipath = rootpath .. '/chai'
+
+if not vim.loop.fs_stat(chaipath) then
+  vim.system({
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/crispybaccoon/chaivim.git',
+    chaipath,
+  }):wait()
+end
+vim.opt.rtp:prepend(chaipath)
+
+require 'core'.setup 'custom'
---
+++ lua/custom/init.lua
@@ -0,0 +1,25 @@
+return {
+  colorscheme = 'evergarden',
+  transparent_background = false,
+  modules = {
+    core = {
+      {
+        'options',
+        opts = {
+          cursorline = false,
+          tab_width = 2,
+          scrolloff = 5,
+        },
+      },
+      {
+        'dash',
+        opts = {
+          open_on_startup = true,
+        },
+      },
+    },
+    custom = {
+      -- your custom modules (in `lua/custom/`)
+    },
+  }
+}
