e701b3bf307fd0be0eb94402bf0e771cc29c4181
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2023-12-18 16:07:00 +0100

    feat(core.config.keymaps): add `M.fmt()` fn; format keymap lhs

--- lua/core/config/keymaps.lua
+++ lua/core/config/keymaps.lua
@@ -25,6 +25,55 @@ _G.Keymap.group = _G.Keymap.group or function(props)
   end
 end
 
+---@param map string
+---@return string
+local function lhs_fmt(map)
+  local leader = vim.g.mapleader == ' ' and 'SPC' or vim.g.mapleader
+  local repl = {
+    ['<leader>'] = leader,
+  }
+
+  local map_str = ''
+
+  local sp_open = 0
+  local temp = ''
+
+  local _map = vim.split(map, '')
+  local i = 1
+  while i <= #_map do
+    temp = ''
+    if sp_open > 0 then
+      if _map[i] == '>' then
+        for pattern, rpl in pairs(repl) do
+          temp = string.gsub(string.sub(map, sp_open, i), pattern, rpl)
+        end
+        sp_open = 0
+      end
+    else
+      if _map[i] == '<' then
+        sp_open = i
+      else
+        if i == #_map and sp_open > 0 then
+          temp = string.sub(map, sp_open, i)
+        else
+          temp = _map[i]
+        end
+      end
+    end
+
+    if temp and #temp > 0 then
+      if #map_str > 0 then
+        map_str = map_str .. ' + '
+      end
+      map_str = map_str .. temp
+    end
+
+    i = i+1
+  end
+
+  return map_str
+end
+
 return {
   setup = function(opts)
     Util.log(string.format('setting leader to "%s"', vim.g.mapleader))
@@ -44,4 +93,5 @@ return {
       Keymap.group { group = group, mappings }
     end
   end,
+  fmt = lhs_fmt,
 }
