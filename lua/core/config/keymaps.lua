a3c35e39e7d8c7e2ec5b95cdd5760d45a40d66cd
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2023-12-18 13:38:17 +0100

    feat(core.config.keymaps): configure groups with `Keymap.group`

--- lua/core/config/keymaps.lua
+++ lua/core/config/keymaps.lua
@@ -1,5 +1,30 @@
 local Util = require 'core.utils'
 
+_G.Keymap = _G.Keymap or {}
+
+_G.Keymap.group = _G.Keymap.group or function(props)
+  if not props.group then
+    Util.log('`Keymap.group` requires `group` field', 'warn')
+    return
+  end
+  local mappings = props[1] or {}
+  if #props > 1 then
+    mappings = props
+  end
+  for _, map in ipairs(mappings) do
+    if #map < 4 then
+      Util.log('`Keymap.group` requires 4 paramaters per keymap', 'warn')
+      goto continue
+    end
+    local mode = map[1]
+    local lhs = map[2]
+    local rhs = map[3]
+    local desc = map[4]
+    keymaps[mode][lhs] = { rhs, desc, group = props.group }
+    ::continue::
+  end
+end
+
 return {
   setup = function(opts)
     Util.log(string.format('setting leader to "%s"', vim.g.mapleader))
@@ -16,17 +41,7 @@ return {
     }
 
     for group, mappings in pairs(opts.mappings) do
-      for _, map in ipairs(mappings) do
-        if #map < 4 then
-          goto continue
-        end
-        local mode = map[1]
-        local lhs = map[2]
-        local rhs = map[3]
-        local desc = map[4]
-        keymaps[mode][lhs] = { rhs, desc, group = group }
-        ::continue::
-      end
+      Keymap.group { group = group, mappings }
     end
   end
 }
