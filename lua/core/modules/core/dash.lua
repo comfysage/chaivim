c1deeb86c9b74f475b0969136b8021f23c9398fd
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2023-12-19 19:55:58 +0100

    feat(core.modules.core.dash): generate buttons based on keymaps

--- lua/core/modules/core/dash.lua
+++ lua/core/modules/core/dash.lua
@@ -13,16 +13,21 @@ return {
         [[   ▝██████████▛ ]],
         [[    ▝▀▀▀▀▀▀▀▀▀  ]],
       },
-      buttons = {
-        { 'find files', core.modules.core.telescope.opts.mappings.find_files, function()
-          require 'core.plugin.telescope'
-              .picker.find_files {}
-        end },
-        { 'select theme', core.modules.core.telescope.opts.mappings.colorscheme, function()
-          require 'telescope.builtin'
-              .colorscheme()
-        end },
-      },
+      buttons = function()
+        local buttons = {
+          core.modules.core.telescope.opts.mappings.find_files,
+          core.modules.core.telescope.opts.mappings.colorscheme,
+        }
+
+        local result = {}
+
+        for i, lhs in ipairs(buttons) do
+          local map = require 'keymaps.data'.get_mapping({ lhs = lhs }) or { desc = '', lhs = '', fn = '' }
+          result[i] = { map.desc, map.lhs, map.fn }
+        end
+
+        return result
+      end,
     },
   },
 }
