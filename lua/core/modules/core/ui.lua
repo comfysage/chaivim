bb91d830855ae3d5947aaa97fcaff36bdf54dca0
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2023-12-21 14:51:28 +0100

    style(core.modules.core.ui): fix formatting

--- lua/core/modules/core/ui.lua
+++ lua/core/modules/core/ui.lua
@@ -6,11 +6,11 @@ return {
         enabled = true,
 
         -- Default prompt string
-        default_prompt = "input:",
-        prompt_separator = ":",
+        default_prompt = 'input:',
+        prompt_separator = ':',
 
         -- Can be 'left', 'right', or 'center'
-        title_pos = "left",
+        title_pos = 'left',
 
         -- When true, <Esc> will close the modal
         insert_only = true,
@@ -19,15 +19,15 @@ return {
         start_in_insert = true,
 
         -- These are passed to nvim_open_win
-        border = "rounded",
+        border = 'rounded',
         -- 'editor' and 'win' will default to being centered
-        relative = "cursor",
+        relative = 'cursor',
 
         -- These can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
         prefer_width = 40,
         width = nil,
         -- min_width and max_width can be a list of mixed types.
-        -- min_width = {20, 0.2} means "the greater of 20 columns or 20% of total"
+        -- min_width = {20, 0.2} means 'the greater of 20 columns or 20% of total'
         max_width = { 140, 0.9 },
         min_width = { 20, 0.2 },
 
@@ -37,7 +37,7 @@ return {
           wrap = false,
           -- Indicator for when text exceeds window
           list = true,
-          listchars = "precedes:…,extends:…",
+          listchars = 'precedes:…,extends:…',
           -- Increase this for more context when text scrolls off the window
           sidescrolloff = 0,
         },
