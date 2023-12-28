4e1204ff7cfe7c56556f653c45d2051f5c842ab0
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2023-12-28 13:57:44 +0100

    add(core.config.base16): create :SelectTheme command using `core.plugin.command`

--- lua/core/config/base16.lua
+++ lua/core/config/base16.lua
@@ -1,8 +1,10 @@
 return {
   setup = function(opts)
-    vim.api.nvim_create_user_command('SelectTheme', function()
-      require 'core.plugin.base16'.select()
-    end, {})
+    require 'core.plugin.command'.create {
+      name = 'SelectTheme', fn = function()
+        require 'core.plugin.base16'.select()
+      end,
+    }
     if opts.colorscheme then
       vim.defer_fn(function()
         require 'core.plugin.base16'.load(opts.colorscheme)
