3b9cc2b869d2eb9dbb3d757f9e391a1d95dc8ca8
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2024-01-01 15:21:46 +0100

    feat(core.lib): create lib fn; create `core.lib.autocmd`; create `core.lib.event`; create `core.lib.keymaps`

--- lua/core/lib/init.lua
+++ lua/core/lib/init.lua
@@ -3,6 +3,26 @@ return {
     core.lib = {}
     require 'core.plugin.icons'.setup()
     require 'core.plugin.hl'.setup()
-    require 'core.plugin.keymaps'
+
+    -- statically allocated instead of dynamically by function wrapping
+    ---@class CoreLib
+    ---@field autocmd CoreLib__autocmd
+    ---@field event CoreLib__event
+    ---@field keymaps CoreLib__keymaps
+
+    ---@class CoreLib__autocmd
+    ---@field create _
+    core.lib.autocmd = {}
+    core.lib.autocmd.create = require 'core.load.handle'.create
+
+    ---@class CoreLib__event
+    ---@field trigger fun(ev: string)
+    core.lib.event = {}
+    core.lib.event.trigger = require 'core.load.handle'.start
+
+    ---@class CoreLib__keymaps
+    ---@field fmt fun(lhs: string): string
+    core.lib.keymaps = {}
+    core.lib.keymaps.fmt = require 'core.plugin.keymaps'.fmt
   end,
 }
