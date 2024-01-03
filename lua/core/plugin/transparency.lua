3b6945eab3ef3ef860607fae7f63d1d14fcc2445
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2024-01-03 15:39:55 +0100

    fix(core.plugin.transparency): fix removing transparency; use `_G.saved_highlights`

--- lua/core/plugin/transparency.lua
+++ lua/core/plugin/transparency.lua
@@ -12,25 +12,38 @@ return {
     core.lib.autocmd.create {
       event = 'ColorScheme', priority = 100,
       fn = function(_)
+        -- reload highlights after colorscheme is switched/reloaded with changes
+        require 'core.plugin.transparency'.create()
         require 'core.plugin.transparency'.fix()
       end,
     }
   end,
+  get = function()
+    return _G.saved_highlights.transparent or require 'core.plugin.transparency'.create()
+  end,
   create = function()
-    return {
-      Normal = { fg = core.lib.hl:get('ui', 'bg').fg, bg = 'none' },
-      SignColumn = glassify 'SignColumn',
-      LineNr = glassify 'LineNr',
-      TabLine = glassify 'TabLine',
-      TabLineFill = glassify 'TabLineFill',
+    _G.saved_highlights = {
+      transparent = {
+        Normal = { fg = core.lib.hl:get('ui', 'bg').fg, bg = 'none' },
+        SignColumn = glassify 'SignColumn',
+        LineNr = glassify 'LineNr',
+        TabLine = glassify 'TabLine',
+        TabLineFill = glassify 'TabLineFill',
+      },
+      normal = {},
     }
+    local save = vim.tbl_keys(_G.saved_highlights.transparent)
+    for _, name in ipairs(save) do
+      _G.saved_highlights.normal[name] = core.lib.hl.get_hl { name = name }
+    end
+    return _G.saved_highlights.transparent
   end,
   ---@param mode boolean
   set = function(mode)
     if mode then
-      core.lib.hl.apply(require 'core.plugin.transparency'.create())
+      core.lib.hl.apply(require 'core.plugin.transparency'.get())
     else
-      require 'core.parts'.colorscheme {}
+      core.lib.hl.apply(_G.saved_highlights.normal)
     end
   end,
   fix = function()
