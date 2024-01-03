f5b02ac9d7b8f678ad26603a76c1c8a86de7c466
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2024-01-03 14:44:09 +0100

    fix(core.config.options): add load_plugins field to OptionsConfig

--- lua/core/config/options.lua
+++ lua/core/config/options.lua
@@ -7,6 +7,7 @@ local M = {}
 ---@field scrolloff integer
 ---@field use_ripgrep boolean
 ---@field treesitter_folds boolean
+---@field load_plugins table<string>
 
 --- Setup options
 ---@param opts OptionsConfig
