37c79ebfb972c1b8dc572cf5d31e3ba2cc7dab34
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2024-01-03 13:19:13 +0100

    fix(core.lib.math): fix `color == 'none'`

--- lua/core/lib/math.lua
+++ lua/core/lib/math.lua
@@ -31,6 +31,9 @@ end
 ---@class CoreLib__math
 ---@field hex_to_rgb fun(n: integer): core.types.lib.color.Color
 function core.lib.math.hex_to_rgb(n)
+  if n == 'none' then
+    n = 0
+  end
   local components = core.lib.math.hex_to_components(3, n)
   return { r = components[1], g = components[2], b = components[3] }
 end
