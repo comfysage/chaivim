64a45ed6f88e4d9b4f224244f4174344ac9224df
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2024-02-14 15:04:39 +0100

    add(core.config.lazy): move default config to `core.modules.core.lazy`

--- lua/core/config/lazy.lua
+++ lua/core/config/lazy.lua
@@ -1,38 +1,5 @@
 local Util = require 'core.utils'
 
-local lazy_config = {
-  ui = {
-    -- a number <1 is a percentage., >1 is a fixed size
-    size = { width = 90, height = 0.8 },
-    wrap = true, -- wrap the lines in the ui
-    -- The border to use for the UI window. Accepts same border values as |nvim_open_win()|.
-    border = 'none',
-    icons = {
-      cmd = "! ",
-      config = core.lib.icons.syntax.constructor,
-      event = core.lib.icons.syntax.event,
-      ft = core.lib.icons.syntax.file,
-      init = core.lib.icons.syntax.constructor,
-      import = core.lib.icons.syntax.reference,
-      keys = core.lib.icons.syntax.snippet,
-      lazy = core.lib.icons.syntax.fn,
-      loaded = core.icons.info.loaded,
-      not_loaded = core.icons.info.not_loaded,
-      plugin = core.icons.syntax.package,
-      runtime = core.icons.syntax.null,
-      source = core.icons.syntax.module,
-      start = core.icons.debug.start,
-      task = core.icons.ui.item_prefix,
-      list = {
-        '-',
-        '*',
-        '*',
-        '-',
-      },
-    },
-  },
-}
-
 local overwrite = {
   root = nil, -- directory where plugins will be installed
   -- required for core bootstrap
@@ -46,8 +13,6 @@ local overwrite = {
 
 return {
   setup = function(opts)
-    opts = opts or {}
-    opts.config = vim.tbl_deep_extend('force', lazy_config, opts.config)
     opts.config = vim.tbl_deep_extend('force', opts.config, overwrite)
 
     -- set lazy path
@@ -62,5 +27,5 @@ return {
     Util.log('lazy.setup', 'loading plugins.')
     ---@diagnostic disable-next-line: redundant-parameter
     require 'lazy'.setup('core.lazy.plugins', opts.config)
-  end
+  end,
 }
--- lua/core/modules/core/lazy.lua
+++ lua/core/modules/core/lazy.lua
@@ -1,7 +1,38 @@
 return {
   default = {
     opts = {
-      config = {},
+      config = {
+        ui = {
+          -- a number <1 is a percentage., >1 is a fixed size
+          size = { width = 90, height = 0.8 },
+          wrap = true, -- wrap the lines in the ui
+          -- The border to use for the UI window. Accepts same border values as |nvim_open_win()|.
+          border = 'none',
+          icons = {
+            cmd = "! ",
+            config = core.lib.icons.syntax.constructor,
+            event = core.lib.icons.syntax.event,
+            ft = core.lib.icons.syntax.file,
+            init = core.lib.icons.syntax.constructor,
+            import = core.lib.icons.syntax.reference,
+            keys = core.lib.icons.syntax.snippet,
+            lazy = core.lib.icons.syntax.fn,
+            loaded = core.lib.icons.info.loaded,
+            not_loaded = core.lib.icons.info.not_loaded,
+            plugin = core.lib.icons.syntax.package,
+            runtime = core.lib.icons.syntax.null,
+            source = core.lib.icons.syntax.module,
+            start = core.lib.icons.debug.start,
+            task = core.lib.icons.ui.item_prefix,
+            list = {
+              '-',
+              '*',
+              '*',
+              '-',
+            },
+          },
+        },
+      },
       module = 'plugins',
     },
   },
