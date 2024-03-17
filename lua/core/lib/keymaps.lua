5eb73a8d8ac63f438afd26c36f83deef714c5b77
comfysage <67917529+comfysage@users.noreply.github.com>
2024-03-17 13:52:18 +0100

    feat(core.lib.keymaps): add `core.lib.keymaps.open_qf_list()` fn

--- lua/core/lib/keymaps.lua
+++ lua/core/lib/keymaps.lua
@@ -2,3 +2,13 @@
 ---@field fmt fun(lhs: string): string
 core.lib.keymaps = {}
 core.lib.keymaps.fmt = require 'core.plugin.keymaps'.fmt
+
+---@class core.types.lib.keymaps
+---@field open_qf_list function
+core.lib.keymaps.open_qf_list = function()
+  if core.lib.options:enabled 'trouble' then
+    require("trouble").toggle("quickfix")
+  else
+    vim.cmd.copen()
+  end
+end
