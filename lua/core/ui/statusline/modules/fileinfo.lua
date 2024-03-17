10fd2eb763f56eb9b7e0829fbcc4b3bb06ebfcc0
comfysage <67917529+comfysage@users.noreply.github.com>
2024-03-17 12:59:32 +0100

    fix(core.ui.statusline.modules.fileinfo): fix devicons; fix formatting

--- lua/core/ui/statusline/modules/fileinfo.lua
+++ lua/core/ui/statusline/modules/fileinfo.lua
@@ -1,20 +1,20 @@
 local utils = require 'core.ui.statusline.utils'
 
 return function()
-  local icon = " 󰈚 "
+  local icon = "󰈚"
   local path = vim.api.nvim_buf_get_name(utils.stbufnr())
-  local name = (path == "" and "empty ") or path:match "([^/\\]+)[/\\]*$"
+  local name = (path == "" and "empty") or path:match "([^/\\]+)[/\\]*$"
 
-  if name ~= "Empty " then
+  if name ~= "empty" and core.config.ui.devicons then
     local devicons_present, devicons = pcall(require, 'nvim-web-devicons')
 
     if devicons_present then
       local ft_icon = devicons.get_icon(name)
-      icon = (ft_icon ~= nil and " " .. ft_icon) or icon
+      if ft_icon then
+        icon = ft_icon
+      end
     end
-
-    name = " " .. name .. " "
   end
 
-  return icon .. name
+  return core.lib.fmt.space(icon) .. name .. " "
 end
